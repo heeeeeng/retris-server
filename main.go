8b7fcf301f6dcd3330cf2d50d7f49d4d583cc86d
heeeeeng <boh_zhu@163.com>
2018-07-20 19:13:46 +0800

    some update

--- main.go
+++ main.go
@@ -1,10 +1,46 @@
 package main
 
 import (
-	
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
+	
+	initURL()
+
+	fmt.Println("---------Server Start!---------")
+	fmt.Println("Port: ", 10000)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+
+func initURL() {
+	http.HandleFunc("/new_game", createNewGame)
+	http.HandleFunc("/user_ctrl", handleUserCtrl)
+}
+
+func createNewGame(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func handleUserCtrl(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
+type Server struct {
+
+}
+
+func NewServer() *Server {
+
+	return nil
+}
+
+func (srv *Server) Start() {}
+
+func (srv *Server) Stop() {}
+
 
